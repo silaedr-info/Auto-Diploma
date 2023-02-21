4c4f642fe555f61b18af9c7bab8282bcf39bd9dd
Alexander <alexandervolodin2020@gmail.com>
2023-02-21 12:06:20 +0300

    Make a transfiguration from url to file

--- main.py
+++ main.py
@@ -1,27 +1,25 @@
 import file_reform
-from flask import Flask, request, render_template, send_file, redirect
+from flask import Flask, request, render_template, send_file, redirect, url_for
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def start():
-    return ''
-
-
-@app.route('/api/upload', methods=['POST', 'GET'])
+@app.route('/', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'POST':
-        f = request.files['data']
-        if f:
-            f.save('data.csv')
-            file_reform.file_reform_from_csv()
+    if request.method == 'GET':
+        api_upload = dict(request.args)
+        if 'project' in api_upload:
+            file_reform.make_diploma(api_upload['template'], int(api_upload['have_project']),
+                                     'special', api_upload['name'],
+                                     api_upload['project'])
         else:
-            f = request.form
-            if f['project'] is not None:
-                file_reform.make_diploma(f['template'], int(f['have_project']), 'special', f['name'], f['project'])
-            else:
-                file_reform.make_diploma(f['template'], int(f['have_project']), 'special', f['name'])
+            file_reform.make_diploma(api_upload['template'], int(api_upload['have_project']),
+                                     'special', api_upload['name'])
+        return redirect('../send')
+    elif request.method == 'POST':
+        if 'file' in request.files:
+            request.files['file'].save('data.csv')
+            file_reform.file_reform_from_csv()
         return redirect('../send')
     return render_template('index.html')
 
