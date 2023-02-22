e4f6b183fa59d768bbc7f0fa100570c29b1b38fd
Alexander <alexandervolodin2020@gmail.com>
2023-02-22 09:58:30 +0300

    Removed bugs and made support more templates

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import file_reform
-from flask import Flask, request, render_template, send_file, redirect, url_for
+import os
+from flask import Flask, request, render_template, send_file, redirect
 
 app = Flask(__name__)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/api/upload', methods=['GET', 'POST'])
 def upload():
-    if request.method == 'GET':
+    if request.args:
         api_upload = dict(request.args)
         if 'project' in api_upload:
             file_reform.make_diploma(api_upload['template'], int(api_upload['have_project']),
@@ -16,17 +17,30 @@ def upload():
             file_reform.make_diploma(api_upload['template'], int(api_upload['have_project']),
                                      'special', api_upload['name'])
         return redirect('../send')
-    elif request.method == 'POST':
-        if 'file' in request.files:
-            request.files['file'].save('data.csv')
+    else:
+        if request.files:
+            request.files['data'].save('data.csv')
             file_reform.file_reform_from_csv()
-        return redirect('../send')
-    return render_template('index.html')
+            return redirect('../send')
+        return render_template('index.html')
 
 
 @app.route('/send', methods=['POST', 'GET'])
 def send():
     return send_file('diplomas.zip')
+
+
+@app.route('/add_new_template', methods=['GET', 'POST'])
+def add_template():
+    if request.method == 'POST':
+        request.files.get('new_template').save(f'templates_pdf/{request.args.get("name_of_template")}')
+    return render_template('add_template.html')
+
+
+@app.route('/delete_zip_file')
+def delete_zip():
+    if os.path.exists('diplomas.zip'):
+        os.remove('diplomas.zip')
 
 
 app.run()
